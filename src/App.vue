125e3ae79b9a7319208e6dfbc95ffdfd4a5d6bec
Tatsumi0000 <ti18027@student.miyazaki-u.ac.jp>
2019-07-08 19:26:21 +0900

    initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,27 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
-  </div>
+  <v-app class="my_backgound">
+    <v-content>
+      <Start/>
+    </v-content>
+  </v-app>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-#nav {
-  padding: 30px;
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-    &.router-link-exact-active {
-      color: #42b983;
-    }
+<script>
+import Start from "./components/Start";
+
+export default {
+  name: "App",
+  components: {
+    Start
+  },
+  data() {
+    return {};
   }
+};
+</script>
+
+<style scoped>
+.my_backgound {
+  background-color: dimgrey;
 }
 </style>
---
+++ src/components/Start.vue
@@ -0,0 +1,59 @@
+<template>
+  <v-container>
+    <v-layout row align-center>
+      <!-- text-XX-centerで中央寄せにする -->
+      <v-flex
+        text-sm-center
+        text-md-center
+        text-lg-center
+        text-xl-center
+        sm4
+        md4
+        lg4
+        xl4
+        pa-3
+        v-for="icon in icons"
+        :key="icon"
+      >
+        <v-btn align-center fab class="btn_size white--text" :color="icon.color">
+          <v-icon>{{ icon.icon_name }}</v-icon>
+        </v-btn>
+      </v-flex>
+    </v-layout>
+  </v-container>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      icons: [
+        { color: "red", icon_name: "play_arrow" },
+        { color: "blue", icon_name: "pause" },
+        { color: "green", icon_name: "settings" }
+      ]
+    };
+  }
+};
+</script>
+
+<style scoped>
+.btn_size {
+  width: 100px;
+  height: 100px;
+}
+
+.play_icon {
+  color: rgba(255, 0, 0, 0.473);
+}
+.play_icon:hover {
+  color: red;
+}
+
+.pause_icon {
+}
+.pause_icon:hover {
+  color: blue;
+}
+</style>
+
